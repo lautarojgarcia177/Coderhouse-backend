c7b2f81115c307ce48666d09c045a627b1e19e19
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-06-09 10:01:26 -0300

    finalizado ejs

---
+++ Motores de plantillas parte 2/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Motor de templates
+app.set('view engine', 'ejs');
+
+// Rutas
+const apiRouter = require('./routes/apiRoutes.js');
+const mvcRouter = require('./routes/mvcRoutes.js');
+app.use('/api', apiRouter);
+app.use('/mvc', mvcRouter);
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+    console.log(`servidor escuchando en http://localhost:${PORT}`);
+});
+
+// en caso de error, avisar
+server.on('error', console.warn);
+
+// Middleware para manejo de errores
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Hubo un error');
+});
+
+module.exports = server;
