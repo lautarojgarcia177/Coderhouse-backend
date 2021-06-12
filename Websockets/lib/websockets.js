8c6b17dc1ba365d1f40752057e1407d955346f2b
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-06-12 12:57:42 -0300

    separo websockets en otro archivo

--- Websockets/lib/websockets.js
+++ Websockets/lib/websockets.js
@@ -1,12 +1,12 @@
 const { Server } = require("socket.io");
 
-// importo la instancia del controlador
+// Importo la instancia del controlador
 const productos = require('../controlador/productos.js');
 
+// Exporto el websocket
 module.exports.setup = function(server) {
     const io = new Server(server);
     io.on('connection', socket => {
-        console.log('a user connected');
         socket.on('insertarProducto', producto => {
             productos.agregarProducto(producto);
             io.emit('actualizarListado', productos.obtenerProductos());
