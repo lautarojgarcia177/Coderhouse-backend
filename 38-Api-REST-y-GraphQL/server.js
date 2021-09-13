678902b6a599558347a7a5bcea5ec70bc86e745c
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-09-13 00:48:01 -0300

    GraphQL

--- 38-Api-REST-y-GraphQL/server.js
+++ 38-Api-REST-y-GraphQL/server.js
@@ -1,45 +1,67 @@
 var express = require("express");
 var { graphqlHTTP } = require("express-graphql");
-var graphql = require("graphql");
+var { buildSchema } = require("graphql");
 var fs = require("fs");
 
 var products = JSON.parse(fs.readFileSync("products.json", "utf8"));
 
-var productType = new graphql.GraphQLObjectType({
-  name: "Product",
-  fields: {
-    id: { type: graphql.GraphQLInt },
-    title: { type: graphql.GraphQLString },
-    price: { type: graphql.GraphQLFloat },
-    thumbnail: { type: graphql.GraphQLString },
-  },
-});
+var schema = buildSchema(`
+type Query {
+  product(id: Int!): Product,
+  products: [Product]  
+},
+type Mutation {
+  insertProduct(id: Int!, title: String, price: Float, thumbnail: String): Product
+},
+type Product {
+  id: Int
+  title: String
+  price: Float
+  thumbnail: String
+}
+`);
 
 
-var queryType = new graphql.GraphQLObjectType({
-  name: "Query",
-  fields: {
-    product: {
-      type: productType,
-      args: {
-        id: { type: graphql.GraphQLInt },
-      },
-      resolve: (_, { id }) => {
-        return products.find((product) => product.id == id);
-      },
-    },
-  },
-});
+var getProductById = function (args) {
+  var id = args.id;
+  return products.find((product) => product.id == id);
+};
 
-var schema = new graphql.GraphQLSchema({ query: queryType });
+var insertProduct = function ({ id, title, thumbnail, price }) {
+  let product = { id, title, thumbnail, price };
+  products.push(product);
+  return product;
+};
+
+var root = {
+  product: getProductById,
+  products: () => products,
+  insertProduct: insertProduct
+};
 
 var app = express();
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
+    rootValue: root,
     graphiql: true,
   })
 );
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.put('/rest/products/update/:id', (req, res, next) => {
+  let productToUpdate = products.find(product => product.id == req.params.id );
+  Object.assign(productToUpdate, req.body);
+  res.json(productToUpdate);
+});
+
+app.delete('/rest/products/delete/:id', (req, res, next) => {
+  products = products.filter(product => product.id != req.params.id);
+  res.send('Producto eliminado con éxito');
+});
+
 app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
