b1d1cf3fbd6386314ff972e7a35bb9ce7ce0249b
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-10-06 00:22:34 -0300

    GraphQL

---
+++ 44-API-GraphQL/server/src/controllers/article.js
@@ -0,0 +1,32 @@
+const daoFactory = require('../dao/DAOFactory');
+const config = require('../config/config');
+
+class ArticleController {
+
+    constructor() {
+        this.articleDao = daoFactory.getPersistence('article', config.PERSISTENCE);
+    }
+
+    async find(query = {}) {
+        return await this.articleDao.find(query);
+    }
+
+    async create(data) {
+        return await this.articleDao.create(data);
+    }
+
+    async findById(id) {
+        let article = await this.articleDao.findById(id);
+        return article.toJSON();
+    }
+
+    async update(id, data) {
+        return await this.articleDao.update(id, data);
+    }
+
+    async delete(id) {
+        return await this.articleDao.delete(id);
+    }
+}
+
+module.exports = new ArticleController();
---
+++ 44-API-GraphQL/server/src/server.js
@@ -0,0 +1,67 @@
+
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const logger = require('morgan');
+const chalk = require('chalk');
+const config = require('./config/config');
+const cors = require('cors');
+
+// load .env configuration
+require('dotenv').config();
+
+const success = chalk.green;
+const error = chalk.red;
+
+// create our app with express.
+const app = express();
+
+// configure all environments.
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const corsOptions = {
+    origin: process.env.ORIGIN,
+    optionSuccessStatus: 200,
+    methods: "GET, PUT, POST, DELETE"
+}
+
+app.use(cors(corsOptions));
+
+// static files
+app.use(express.static('public'));
+
+// import all routes
+const index = require('./routes/index');
+app.use(index);
+
+if (config.ENV === 'development') {
+    app.use(logger('dev'));
+    const graphiql = require('./routes/graphql');
+    app.use('/articles', graphiql.start());
+} else {
+    const articleRouter = require('./routes/articles');
+    app.use('/api', articleRouter);
+}
+
+// catch all errors.
+app.use(function (err, req, res, next) {
+    console.log(error(err.message));
+    res.status(500).json({ code: 500, message: err.message });
+});
+
+// catch unhandled rejection from promises.
+process.on('unhandledRejection', function (err) {
+    console.log(error(err.stack));
+});
+
+// set host and port.
+app.set('host', config.HOST);
+app.set('port', config.PORT);
+
+// listening application.
+app.listen(app.get('port'), async () => {
+    console.log(success(`[server] - started in ${app.get('host')}:${app.get('port')} in ${config.ENV} - GRAPHIQL: ${config.GRAPHIQL}`));
+});
