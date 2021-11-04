1e52db13a6435b85bdb02e70e842255489f18ed1
Lautaro Garcia <lautarojgarcia177@gmail.com>
2021-11-04 13:14:09 -0300

    Send error email

--- Proyecto-Final/src/config/config.js
+++ Proyecto-Final/src/config/config.js
@@ -16,6 +16,7 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    ERROR_EMAIL_ADDRESS_TO_SEND: Joi.string().description('email address to send errors')
   })
   .unknown();
 
@@ -51,5 +52,6 @@ module.exports = {
       },
     },
     from: envVars.EMAIL_FROM,
+    errorEmailAddressToSend: envVars.ERROR_EMAIL_ADDRESS_TO_SEND
   },
 };
--- Proyecto-Final/src/services/email.service.js
+++ Proyecto-Final/src/services/email.service.js
@@ -23,41 +23,26 @@ const sendEmail = async (to, subject, text) => {
   await transport.sendMail(msg);
 };
 
-/**
- * Send reset password email
- * @param {string} to
- * @param {string} token
- * @returns {Promise}
- */
-const sendResetPasswordEmail = async (to, token) => {
-  const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
-  await sendEmail(to, subject, text);
-};
 
 /**
- * Send verification email
- * @param {string} to
- * @param {string} token
+ * Send error email
+ * @param {object} error
  * @returns {Promise}
  */
-const sendVerificationEmail = async (to, token) => {
-  const subject = 'Email Verification';
+const sendErrorEmail = async (error) => {
+  const subject = 'Server Error in Lautaro Garcia Coderhouse';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  const to = config.email.errorEmailAddressToSend;
+  const text = `
+      There was an error in Lautaro Garcia Coderhouse,
+        id: ${error.code}
+        detail: ${error.message}
+    `;
   await sendEmail(to, subject, text);
 };
 
 module.exports = {
   transport,
   sendEmail,
-  sendResetPasswordEmail,
-  sendVerificationEmail,
+  sendErrorEmail
 };
